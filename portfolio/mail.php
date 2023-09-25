cdb371972da6b5ce5b7ab68655f6045a58d26b99
THEmad71 <mrbangladesh19521971@gmail.com>
2023-09-26 01:01:24 +0600

    just check

---
+++ portfolio/mail.php
@@ -0,0 +1,32 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get data from form
+    $name = $_POST['name'];
+    $email = $_POST['emailaddress'];
+    $emailsub = $_POST['emailsub'];
+    $message = $_POST['message'];
+    $number = $_POST['mobile'];
+
+    // Recipient email address
+    $to = "recipient@example.com"; // Replace with your recipient's email address
+
+    // Email subject
+    $subject = "New Contact Form Submission: $emailsub";
+
+    // Email message
+    $email_message = "Name: $name\r\nEmail: $email\r\nMobile: $number\r\nMessage:\r\n$message";
+
+    // Additional headers
+    $headers = "From: $email\r\n";
+
+    // Send the email
+    if (mail($to, $subject, $email_message, $headers)) {
+        header("Location: thankyou.html"); // Redirect to thank you page
+        exit();
+    } else {
+        echo "Oops! Something went wrong. Please try again later.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
